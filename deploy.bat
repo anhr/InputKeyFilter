2a184e3af5cdbcb640916133b89b93744f9bece9
Andrej <anhr@mail.ru>
2015-07-16 08:38:08 +0700

    Cosmetic changes

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+rem run as administrator
+
+cd /D "G:\My documents\MyProjects\trunk\WebFeatures\WebFeatures\GoogleSite\InputKeyFilter"
+set jsFolderDest=C:\inetpub\wwwroot\GoogleSite\InputKeyFilter\
+
+xcopy index.htm %jsFolderDest%index.htm /Y
+
+pause
+
