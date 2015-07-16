02be563d7d9e1d103b372c752ba9438cd414dc7f
Andrej <anhr@mail.ru>
2015-07-16 08:42:21 +0700

    Delete deploy.bat

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-rem run as administrator
-
-cd /D "G:\My documents\MyProjects\trunk\WebFeatures\WebFeatures\GoogleSite\InputKeyFilter"
-set jsFolderDest=C:\inetpub\wwwroot\GoogleSite\InputKeyFilter\
-
-xcopy index.htm %jsFolderDest%index.htm /Y
-
-pause
-
