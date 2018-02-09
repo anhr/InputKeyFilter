f2d0d4a7f343a1059fde7ee4a1e48431fcf0e5c2
Andrej <anhr@mail.ru>
2018-02-09 12:10:35 +0700

    Cosnetic changes

--- InputKeyFilter.js
+++ InputKeyFilter.js
@@ -2,8 +2,9 @@
  * A Javascript object of the cross-browser filter of value of the text input element on your web page using JavaScript language. You can filter the value as an integer number, a float number etc. , or write a custom filter, such as a phone number filter.
  * Author: Andrej Hristoliubov
  * email: anhr@mail.ru
- * About me: http://anhr.ucoz.net/AboutMe/
+ * About me: http://anhr.github.io/AboutMe/
  * source: https://github.com/anhr/InputKeyFilter
+ * example: http://anhr.github.io/InputKeyFilter/
  * Licences: GPL, The MIT License (MIT)
  * Copyright: (c) 2015 Andrej Hristoliubov
  *
