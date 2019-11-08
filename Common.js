b86dedfe49428e0974272efe8b879cbc162b8210
Andrej <anhr@mail.ru>
2019-11-09 06:17:58 +0700

    link

--- Common.js
+++ Common.js
@@ -2,7 +2,7 @@
  * Common Javascript code. I use it in my different projects
  * Author: Andrej Hristoliubov
  * email: anhr@mail.ru
- * About me: http://anhr.ucoz.net/AboutMe/
+ * About me: https://anhr.github.io/AboutMe/
  * source: https://github.com/anhr/WebFeatures
  * Licences: GPL, The MIT License (MIT)
  * Copyright: (c) 2015 Andrej Hristoliubov
